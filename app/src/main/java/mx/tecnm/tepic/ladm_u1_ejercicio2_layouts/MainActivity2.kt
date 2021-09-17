fbe7445aeec3ff67fdf8e7a07f642fe1c6aaef8d
Fer <femiramontesal@ittepic.edu.mx>
2021-09-16 22:17:55 -0600

    Ejercicio Terminado

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u1_ejercicio2_layouts/MainActivity2.kt
@@ -0,0 +1,16 @@
+package mx.tecnm.tepic.ladm_u1_ejercicio2_layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+
+    }
+}
